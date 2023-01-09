95ba866a7a7e0ae626e32be27b988aee085382eb
sty00A4 <99866356+sty00A4@users.noreply.github.com>
2023-01-09 16:15:46 +0100

    main input file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 #![allow(unused)]
 mod scanning;
 mod compiler;
+use std::env;
 use scanning::{tokens, lexer, nodes, parser};
 use compiler::{types, check};
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let mut args = args.iter();
+    args.next();
+    let Some(input_path) = args.next() else {
+        println!("USAGE: luo [input path] ([output path])");
+        return
+    };
+    let output_path = args.next();
+    // lexing
+    // parsing
+    // type checking
+    // compilation
 }
