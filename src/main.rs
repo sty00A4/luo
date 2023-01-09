8db19b36236731363fdddccd4f28a2d1de794ebd
sty00A4 <99866356+sty00A4@users.noreply.github.com>
2023-01-09 16:04:27 +0100

    setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#![allow(unused)]
+mod scanning;
+use scanning::{tokens, lexer, nodes, parser};
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/scanning/mod.rs
@@ -0,0 +1,4 @@
+pub mod tokens;
+pub mod lexer;
+pub mod nodes;
+pub mod parser;
